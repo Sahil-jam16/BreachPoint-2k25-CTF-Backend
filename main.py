e5c1b8f9746580e77e57022c7b7bf3754aeeba23
SK Sahil Jamadar <sahiljamadar16@gmail.com>
2025-09-28 15:19:34 +0530

    Update CORS configuration to specify allowed origins and remove wildcard entry

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:8081",
     "https://breachpoint-backend-api.onrender.com",
-    '*'
+    "https://breachpoint-2k25-ctf.pages.dev/"
 ]
 
 app.add_middleware(
