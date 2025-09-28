9862b88964cdf939cfbdce8943e61c47ea6290f6
SK Sahil Jamadar <sahiljamadar16@gmail.com>
2025-09-28 16:01:28 +0530

    Update CORS configuration to remove wildcard entry and specify allowed origins

--- main.py
+++ main.py
@@ -12,12 +12,10 @@ app = FastAPI(
 # This list only contains the URLs of the frontends that need access.
 origins = [
     "http://localhost:8081",  # Your local frontend dev server
-    "*"
-    # "http://localhost:5173",
-    # "http://127.0.0.1:8081",
-    # "https://breachpoint-backend-api.onrender.com",
-    # "https://breachpoint-2k25-ctf.pages.dev/"
-    
+    "http://localhost:5173",
+    "http://127.0.0.1:8081",
+    "https://breachpoint-backend-api.onrender.com",
+    "https://breachpoint-2k25-ctf.pages.dev"
 ]
 
 app.add_middleware(
